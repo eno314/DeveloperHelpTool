55d8860faa54e686bdf9747b11110d373d288f80
hiroto kitamura <kirikonknow@gmail.com>
2021-07-25 16:39:47 +0900

    fix type error

--- src/components/molecules/UrlEncodForm/UrlEncodeForm.tsx
+++ src/components/molecules/UrlEncodForm/UrlEncodeForm.tsx
@@ -2,21 +2,21 @@ import React from "react";
 
 class UrlEncodeForm extends React.Component {
 
-    private decodedTextArea = React.createRef<HTMLInputElement>()
-    private encodedTextArea = React.createRef<HTMLInputElement>()
+    private decodedTextArea = React.createRef<HTMLTextAreaElement>()
+    private encodedTextArea = React.createRef<HTMLTextAreaElement>()
 
     render() {
         return (
             <div>
                 <div>
-                    <textarea ref={this.decodedTextArea} rows="4" cols="100" />
+                    <textarea ref={this.decodedTextArea} rows={4} cols={100} />
                 </div>
                 <div>
                     <button onClick={this.onClickUrlEncode.bind(this)}>▼URLエンコード</button>
                     <button onClick={this.onClickUrlDecode.bind(this)}>▲URLデコード</button>
                 </div>
                 <div>
-                    <textarea ref={this.encodedTextArea} rows="4" cols="100" />
+                    <textarea ref={this.encodedTextArea} rows={4} cols={100} />
                 </div>
             </div>
         )
