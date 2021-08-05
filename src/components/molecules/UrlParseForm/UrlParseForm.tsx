ef2266e92da8b939c08a12893fd7a983143e94a6
Hiroto Kitamura <kirikonknow@gmail.com>
2021-08-05 17:31:03 +0900

    BuildURLの機能を追加

--- src/components/molecules/UrlParseForm/UrlParseForm.tsx
+++ src/components/molecules/UrlParseForm/UrlParseForm.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from "react";
 
 type State = {
+    parsedUrlText: string,
+    baseUrlText: string,
     urlParams?: UrlParam[],
 }
 
@@ -21,17 +23,20 @@ class UrlParseForm extends React.Component<{}, State> {
         },
     };
 
-    private readonly parsedUrlText = React.createRef<HTMLInputElement>()
-    private readonly baseUrlText = React.createRef<HTMLInputElement>()
-
     constructor(prop: any) {
         super(prop)
         this.state = {
+            parsedUrlText: '',
+            baseUrlText: '',
             urlParams: [],
         }
 
+        this.onChangeParsedURLText = this.onChangeParsedURLText.bind(this)
+        this.onChangeBaseURLText = this.onChangeBaseURLText.bind(this)
+        this.onChangeParamsText = this.onChangeParamsText.bind(this)
         this.onClickUrlParse = this.onClickUrlParse.bind(this)
         this.onClickUrlBuild = this.onClickUrlBuild.bind(this)
+        this.onClickDeleteParam = this.onClickDeleteParam.bind(this)
     }
 
     render(): ReactNode {
@@ -39,7 +44,7 @@ class UrlParseForm extends React.Component<{}, State> {
             <div className={"container"}>
                 <div className={"input-group mb-3"}>
                     <span className={"input-group-text"} id={"parsedUrl"}>Parsed URL</span>
-                    <input ref={this.parsedUrlText} type={"url"} className={"form-control"} aria-describedby={"parsedUrl"} />
+                    <input type={"url"} className={"form-control"} aria-describedby={"parsedUrl"} value={this.state.parsedUrlText} onChange={this.onChangeParsedURLText} />
                 </div>
                 <div className={"row"} style={this.styles.primaryButtonsRow}>
                     <div className={"col text-center"}>
@@ -52,13 +57,13 @@ class UrlParseForm extends React.Component<{}, State> {
                 <div>
                     <div className={"input-group mb-3"}>
                         <span className={"input-group-text"} id={"baseUrl"}>Base URL</span>
-                        <input ref={this.baseUrlText} type={"url"} className={"form-control"} aria-describedby={"baseUrl"} />
+                        <input type={"url"} className={"form-control"} aria-describedby={"baseUrl"} value={this.state.baseUrlText} onChange={this.onChangeBaseURLText} />
                     </div>
                     <table className={"table table-sm"}>
                         <thead>
                             <tr>
-                                <th scope={"col"}>URL PARAMS KEY</th>
-                                <th scope={"col"}>URL PARAMS VALUE</th>
+                                <th scope={"col"}>URL PARAM KEY</th>
+                                <th scope={"col"}>URL PARAM VALUE</th>
                                 <th scope={"col"}>DELETE</th>
                             </tr>
                         </thead>
@@ -78,17 +83,38 @@ class UrlParseForm extends React.Component<{}, State> {
         return this.state.urlParams.map((urlParam: UrlParam, i: number) => {
             return (
                 <tr key={i}>
-                    <td><input type={"text"} defaultValue={urlParam.key} /></td>
-                    <td><input type={"text"} defaultValue={urlParam.value} /></td>
-                    <td><button type={"button"} className={"btn btn-secondary btn-sm"}>delete</button></td>
+                    <td><input type={"text"} value={this.state.urlParams[i].key} data-index={i} data-type={"key"} onChange={this.onChangeParamsText} /></td>
+                    <td><input type={"text"} value={this.state.urlParams[i].value} data-index={i} data-type={"value"} onChange={this.onChangeParamsText} /></td>
+                    <td><button type={"button"} className={"btn btn-secondary btn-sm"} onClick={this.onClickDeleteParam} data-index={i}>delete</button></td>
                 </tr>
             )
         })
     }
 
+    onChangeParsedURLText(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            parsedUrlText: e.target.value
+        })
+    }
+
+    onChangeBaseURLText(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            baseUrlText: e.target.value
+        })
+    }
+
+    onChangeParamsText(e: React.ChangeEvent<HTMLInputElement>) {
+        const newUrlParams = Array.from(this.state.urlParams)
+        const index = Number(e.currentTarget.getAttribute('data-index'))
+        const type = Number(e.currentTarget.getAttribute('data-type'))
+        newUrlParams[index][type] = e.target.value
+        this.setState({
+            urlParams: newUrlParams,
+        })
+    }
+
     onClickUrlParse(e: React.MouseEvent<HTMLElement>) {
-        const parsedUrl = new URL(this.parsedUrlText.current.value)
-        this.baseUrlText.current.value = `${parsedUrl.protocol}//${parsedUrl.hostname}${parsedUrl.pathname}`
+        const parsedUrl = new URL(this.state.parsedUrlText)
 
         const urlParams: UrlParam[] = []
         parsedUrl.searchParams.forEach(((value: string, key: string, parent: URLSearchParams) => {
@@ -96,15 +122,30 @@ class UrlParseForm extends React.Component<{}, State> {
         }))
 
         this.setState({
+            baseUrlText: `${parsedUrl.protocol}//${parsedUrl.hostname}${parsedUrl.pathname}`,
             urlParams: urlParams,
         })
     }
 
     onClickUrlBuild(e: React.MouseEvent<HTMLElement>) {
+        const params = this.state.urlParams
+            .map((urlParam: UrlParam) => {
+                const encodedValue = encodeURIComponent(urlParam.value)
+                return `${urlParam.key}=${encodedValue}`
+            })
+            .join('&')
+
+        this.setState({
+            parsedUrlText: `${this.state.baseUrlText}?${params}`
+        })
     }
 
-    createParamsTr(paramKey: string, paramValue: string): string {
-        return
+    onClickDeleteParam(e: React.MouseEvent<HTMLElement>) {
+        const clickButtonIndex = e.currentTarget.getAttribute('data-index')
+        const newUrlParams = this.state.urlParams.filter((urlParam: UrlParam, index: number) => clickButtonIndex != index.toString())
+        this.setState({
+            urlParams: newUrlParams,
+        })
     }
 }
 
