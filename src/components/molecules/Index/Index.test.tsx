59348f15f713dc4e50cfd9a1a224e12246dab43e
Hiroto Kitamura <kirikonknow@gmail.com>
2021-08-09 21:17:57 +0900

    support typescript for jest

---
+++ src/components/molecules/Index/Index.test.tsx
@@ -0,0 +1,8 @@
+import Index from "./Index";
+import { render } from '@testing-library/react';
+
+describe('Index', () => {
+    test('renders Index component', () => {
+        render(<Index />);
+    });
+});
