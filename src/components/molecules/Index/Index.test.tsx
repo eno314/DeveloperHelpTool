0fc49d390ae54b7ef1d0e159489badfeff996698
Hiroto Kitamura <kirikonknow@gmail.com>
2021-08-12 11:27:12 +0900

    fix index unit test.

--- src/components/molecules/Index/Index.test.tsx
+++ src/components/molecules/Index/Index.test.tsx
@@ -8,6 +8,8 @@ import { render, screen } from '@testing-library/react';
 describe('Index', () => {
     test('renders Index component', () => {
         render(<Index />);
-        expect(screen.getAllByRole("link")).toHaveLength(2);
+        const links = screen.getAllByRole('link')
+        expect(links[0]).toHaveAttribute('href', '/url/parse');
+        expect(links[1]).toHaveAttribute('href', '/url/encode');
     });
 });
