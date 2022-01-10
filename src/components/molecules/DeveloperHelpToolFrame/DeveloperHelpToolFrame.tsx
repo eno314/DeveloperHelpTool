8f03fb170b9535384b42ff75df607770f826c157
Hiroto Kitamura <kirikonknow@gmail.com>
2022-01-10 13:01:02 +0900

    fix DeveloperHelperToolFram to Function Component.

--- src/components/molecules/DeveloperHelpToolFrame/DeveloperHelpToolFrame.tsx
+++ src/components/molecules/DeveloperHelpToolFrame/DeveloperHelpToolFrame.tsx
@@ -1,28 +1,25 @@
 import Head from "next/head";
-import React, { ReactNode } from "react";
+import React from "react";
 
 type Props = {
     subTitle: string;
     content: React.ReactNode
 }
 
-class DeveloperHelpToolFrame extends React.Component<Props, {}> {
+const DeveloperHelpToolFrame = (({ subTitle, content }: Props) => {
+    return (
+        <div>
+            <Head>
+                <title>Developer Help Tool - {subTitle}</title>
+            </Head>
 
-    render(): ReactNode {
-        return (
-            <div>
-                <Head>
-                    <title>Developer Help Tool - {this.props.subTitle}</title>
-                </Head>
-
-                <main>
-                    <h1 className={"col text-center"}>Developer Help Tool</h1>
-                    <h2 className={"col text-center"}>{this.props.subTitle}</h2>
-                    {this.props.content}
-                </main>
-            </div>
-        )
-    }
-}
+            <main>
+                <h1 className={"col text-center"}>Developer Help Tool</h1>
+                <h2 className={"col text-center"}>{subTitle}</h2>
+                {content}
+            </main>
+        </div>
+    )
+})
 
 export default DeveloperHelpToolFrame
