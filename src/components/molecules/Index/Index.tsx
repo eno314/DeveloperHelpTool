7dbf7cf9b4ce96efb1c6874327cd8276cad89087
Hiroto Kitamura <kirikonknow@gmail.com>
2022-01-10 11:04:38 +0900

    fix index to Function Components.

--- src/components/molecules/Index/Index.tsx
+++ src/components/molecules/Index/Index.tsx
@@ -1,38 +1,37 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import Link from 'next/link'
 
-class Index extends React.Component {
+const links = [
+    {
+        path: '/string/replace',
+        title: 'String Replace Tool',
+        description: 'You can replace string by this tool.',
+    },
+    {
+        path: '/url/encode',
+        title: 'Url Encode And Decode Tool',
+        description: 'You can encode and decode url by this tool.',
+    },
+    {
+        path: '/url/parse',
+        title: 'Url Parse Tool',
+        description: 'You can parse and build url by this tool.',
+    },
+]
 
-    render(): ReactNode {
-        return (
-            <div className={"list-group"}>
-                <Link href="/string/replace">
-                    <a className={"list-group-item list-group-item-action"}>
-                        <div className={"d-flex justify-content-between"}>
-                            <h5 className="mb-1">String Replace Tool</h5>
-                        </div>
-                        <p className="mb-1">You can replace string by this tool.</p>
-                    </a>
-                </Link>
-                <Link href="/url/encode">
-                    <a className={"list-group-item list-group-item-action"}>
-                        <div className={"d-flex justify-content-between"}>
-                            <h5 className="mb-1">Url Encode And Decode Tool</h5>
-                        </div>
-                        <p className="mb-1">You can encode and decode url by this tool.</p>
-                    </a>
-                </Link>
-                <Link href="/url/parse">
-                    <a className={"list-group-item list-group-item-action"}>
-                        <div className={"d-flex justify-content-between"}>
-                            <h5 className="mb-1">Url Parse Tool</h5>
-                        </div>
-                        <p className="mb-1">You can parse and build url by this tool.</p>
-                    </a>
-                </Link>
-            </div>
-        )
-    }
-}
+const Index = () => <div className={"list-group"}>{linkList()}</div>
+
+const linkList = () => links.map((link) => {
+    return (
+        <Link href={link.path}>
+            <a className={"list-group-item list-group-item-action"}>
+                <div className={"d-flex justify-content-between"}>
+                    <h5 className="mb-1">{link.title}</h5>
+                </div>
+                <p className="mb-1">{link.description}</p>
+            </a>
+        </Link>
+    )
+})
 
 export default Index
